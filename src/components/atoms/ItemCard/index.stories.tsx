2b90600f5f3ad5d6326b08bad0af672d3f08f606
arfes0e2b3c <webyata@gmail.com>
2023-06-12 17:58:33 +0900

    feat: ItemCardの追加

---
+++ src/components/atoms/ItemCard/index.stories.tsx
@@ -0,0 +1,18 @@
+import { StoryObj, Meta } from '@storybook/react';
+import { ItemCard } from '.';
+
+const meta: Meta<typeof ItemCard> = {
+  title: 'atoms/ItemCard',
+  component: ItemCard,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ItemCard>;
+export const Default: Story = {
+  args: {
+    id: '57c3ff77-d8bd-41bb-86e3-4526e1b2186c',
+    name: 'ボールペン',
+    thumbnail: 'https://picsum.photos/300/200',
+  },
+};
---
+++ src/components/atoms/ItemCard/index.tsx
@@ -0,0 +1,51 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { FC } from 'react';
+import { tb } from 'src/styles/media';
+import styled from 'styled-components';
+
+const ItemCardContainer = styled.div`
+  width: 244px;
+  border-radius: 8px;
+  overflow: hidden;
+  box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.25);
+  object-fit: cover;
+  ${tb`
+    width: 200px;
+  `}
+`;
+
+const ItemImage = styled(Image)`
+  vertical-align: top;
+  width: 100%;
+  height: auto;
+  aspect-ratio: 1;
+`;
+
+const ItemTitle = styled.div`
+  font-size: 18px;
+  margin: 8px;
+  ${tb`
+    margin: 4px;
+  `}
+`;
+
+type ItemCardProps = {
+  id: string;
+  name: string;
+  thumbnail: string;
+};
+
+export const ItemCard: FC<ItemCardProps> = ({ name, id, thumbnail }) => (
+  <Link href={`/detail/${id}`}>
+    <ItemCardContainer>
+      <ItemImage
+        src={thumbnail}
+        alt="商品のサムネイル画像"
+        width={244}
+        height={244}
+      />
+      <ItemTitle>{name}</ItemTitle>
+    </ItemCardContainer>
+  </Link>
+);
