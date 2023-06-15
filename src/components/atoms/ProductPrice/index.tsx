82a819d811949e2a3d2d0ceefc020070de32cf1f
arfes0e2b3c <webyata@gmail.com>
2023-06-15 17:02:57 +0900

    feat: レスポンシブ対応

--- src/components/atoms/ProductPrice/index.tsx
+++ src/components/atoms/ProductPrice/index.tsx
@@ -1,22 +1,32 @@
 import { FC } from 'react';
 import { colors, fontSizes, fonts } from 'src/styles/Tokens';
+import { tb } from 'src/styles/media';
 import styled from 'styled-components';
 
 const Container = styled.section`
   display: flex;
   align-items: flex-end;
   column-gap: 8px;
+  ${tb`
+    column-gap: 4px;
+  `}
 `;
 
 const TaxExcluded = styled.div`
   font-size: ${fontSizes.fontSize24};
   font-style: ${fonts.NotoSansJP};
+  ${tb`
+    font-size: ${fontSizes.fontSize14};
+  `}
 `;
 
 const DiscountedOriginalPrice = styled.div`
   color: #aaa;
   font-size: ${fontSizes.fontSize24};
   font-style: ${fonts.Roboto};
+  ${tb`
+    font-size: ${fontSizes.fontSize14};
+  `}
 `;
 
 const DiscountedSellingPrice = styled.div`
@@ -24,12 +34,18 @@ const DiscountedSellingPrice = styled.div`
   font-size: ${fontSizes.fontSize36};
   font-style: ${fonts.Roboto};
   line-height: 1.2;
+  ${tb`
+    font-size: ${fontSizes.fontSize24};
+  `}
 `;
 
 const DiscountedYen = styled.div`
   color: ${colors.Red};
   font-size: ${fontSizes.fontSize24};
   font-style: ${fonts.NotoSansJP};
+  ${tb`
+    font-size: ${fontSizes.fontSize14};
+  `}
 `;
 
 const NormalSellingPrice = styled.div`
@@ -37,12 +53,18 @@ const NormalSellingPrice = styled.div`
   font-size: ${fontSizes.fontSize36};
   font-style: ${fonts.Roboto};
   line-height: 1.2;
+  ${tb`
+    font-size: ${fontSizes.fontSize24};
+  `}
 `;
 
 const NormalYen = styled.div`
   color: ${colors.Black};
   font-size: ${fontSizes.fontSize24};
   font-style: ${fonts.NotoSansJP};
+  ${tb`
+    font-size: ${fontSizes.fontSize14};
+  `}
 `;
 
 type ProductPriceProps = {
