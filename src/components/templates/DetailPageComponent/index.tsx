480b2777fab88b1cfdd1ba65307463551cd96072
arfes0e2b3c <webyata@gmail.com>
2023-06-16 15:26:13 +0900

    feat: スタイルの追加

--- src/components/templates/DetailPageComponent/index.tsx
+++ src/components/templates/DetailPageComponent/index.tsx
@@ -3,36 +3,60 @@ import { ProductPrice } from 'src/components/atoms/ProductPrice';
 import { ProductTag } from 'src/components/atoms/ProductTag';
 // eslint-disable-next-line import/no-cycle
 import { DetailPageProps } from 'src/pages/detail/[id]';
-import { Container } from 'src/styles/Home';
+import { colors, fontSizes, fonts, spacingSizes } from 'src/styles/Tokens';
+import { mb, tb } from 'src/styles/media';
+import styled from 'styled-components';
+
+const TitleSection = styled.section`
+  padding: ${spacingSizes.lg};
+  background-color: ${colors.YumeWhiteGreen};
+  ${tb`
+    padding: ${spacingSizes.sm} ${spacingSizes.md};
+  `}
+  ${mb`
+    padding: ${spacingSizes.sm};
+  `}
+`;
+
+const Title = styled.h1`
+  font-size: ${fontSizes.fontSize32};
+  font-style: ${fonts.NotoSansJP};
+  ${tb`
+    font-size: ${fontSizes.fontSize24};
+  `}
+`;
+
+const InfoContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  margin-top: 22px;
+`;
 
 export const DetailPageComponent: FC<{ props: DetailPageProps }> = ({
   props,
 }) => {
   const { id, name, thumbnail, price, tags } = props;
   return (
-    <Container>
-      <section>
+    <TitleSection>
+      <Title>{name}</Title>
+      <InfoContainer>
         <div>
-          <h1>{name}</h1>
+          {tags.map((tag) => (
+            <ProductTag
+              key={tag.id}
+              path={`/detail/${tag.id}`}
+              name={tag.name}
+              color={tag.color}
+            />
+          ))}
         </div>
-        <div>
-          <div>
-            {tags.map((tag) => (
-              <ProductTag
-                key={tag.id}
-                path={`/detail/${tag.id}`}
-                name={tag.name}
-                color={tag.color}
-              />
-            ))}
-          </div>
-          <ProductPrice
-            discounted={price.discounted}
-            originalPrice={price.original_price}
-            sellingPrice={price.selling_price}
-          />
-        </div>
-      </section>
-    </Container>
+        <ProductPrice
+          discounted={price.discounted}
+          originalPrice={price.original_price}
+          sellingPrice={price.selling_price}
+        />
+      </InfoContainer>
+    </TitleSection>
   );
 };
